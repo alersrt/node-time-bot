226825a87463ed95a33010ebec50a4e4e9754790
alersrt <strtmg@yandex.ru>
2018-09-15 01:56:51 +0700

    Add inline functionality

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,34 @@
-const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = process.env.TELEGRAM_TOKEN;
+const uuidv1 = require('uuid/v1');
+
+const eventTypes = require('./constants/eventTypes');
+const TelegramBot = require('node-telegram-bot-api');
+
 const bot = new TelegramBot(TOKEN, {polling: true});
 
-bot.on('message', (msg) => {
+bot.on(eventTypes.MESSAGE, msg => {
   const chatId = msg.chat.id;
 
   return bot.sendMessage(chatId, 'I see you');
+});
+
+bot.on(eventTypes.INLINE_QUERY, query => {
+  let q_id = query.id;
+  let answer = [];
+  let id = uuidv1();
+
+  let text = {
+    id: id,
+    type: 'article',
+    title: 'Dino about',
+    input_message_content: {
+      message_text: 'There is a dino which is saying "I love you!"...',
+    },
+  };
+  answer.push(text);
+  return bot.answerInlineQuery(q_id, answer);
+});
+
+bot.on(eventTypes.CHOSEN_INLINE_RESULT, query => {
+  return console.log(query);
 });
---
+++ src/app/constants/eventTypes.js
@@ -0,0 +1,3 @@
+module.exports.MESSAGE = 'message';
+module.exports.INLINE_QUERY = 'inline_query';
+module.exports.CHOSEN_INLINE_RESULT = 'chosen_inline_result';
