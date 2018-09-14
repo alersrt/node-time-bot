12f6f447b0ce6def3709c7b0325c2cb9018412a9
alersrt <strtmg@yandex.ru>
2018-09-14 22:29:29 +0700

    The trying of the bot's starting

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  return bot.sendMessage(chatId, 'I see you');
+});
